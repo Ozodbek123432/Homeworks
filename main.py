a22a07bddefebb90008b40a8905bf9c65f2e786f
ozodbek <sabdumajid91@gmail.com>
2025-06-12 15:11:59 +0500

    Finish the task

---
+++ main.py
@@ -0,0 +1,40 @@
+l = [1,2,3,4,5]
+for i in l:
+    print(i)
+
+print("vazifa")
+
+l1 = "salom"
+for i in l:
+    print(i)
+
+print("vazifa")
+
+meva = ["olma","banan", "uzum"]
+print(meva)
+
+print("vazifa")
+
+l2 = [1,2,3,4,5,6,7,8,9,10]
+r = [1,3,5,7,9]
+for i in l2:
+    if i in r:
+        print(f"{i} raqami tog")
+
+print("vazifa")
+
+n = int(input("son kriniting:"))
+d = n ** 2
+print(f"natija{d}")
+
+print("vazifa")
+
+n2 = int(input("son kriniting:"))
+n1 = int(input("son kriniting:"))
+d = n1 + n2
+print(f"natija{d}")
+
+print("vazifa")
+
+r1 = ["ali", "vali","hasan"]
+print(r1.index("hasan"))
