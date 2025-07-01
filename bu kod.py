d76d7bc01987f550565f9840722c8ac1ff80abf0
ozodbek <sabdumajid91@gmail.com>
2025-07-01 14:54:59 +0500

    bu kod

--- bu kod.py
+++ bu kod.py
@@ -1,9 +1,13 @@
 
-a = input("raqam kiring  ")
-b = input("isim kiring")
-l = {}
-s = input("malumoot qidirish")
-l [b] = a
-for x, v in l.items():
-    if x == s:
-        print(v)
+l = {
+    "sas": 27421347,
+}
+while True:
+     s = input("malumoot qidirish")
+     if s not in l:
+         b = input("maluumot topilmadi isim kiring")
+         a = input("raqam kiring  ")
+         l [b] = a
+     for x, v in l.items():
+         if x == s:
+             print(v)
