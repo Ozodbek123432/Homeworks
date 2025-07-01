ae29aab7ebda04d320c2ba43790616418bfa9c39
ozodbek <sabdumajid91@gmail.com>
2025-07-01 14:14:49 +0500

    bu kod

--- main.py
+++ main.py
@@ -1,40 +1,35 @@
-l = [1,2,3,4,5]
-for i in l:
-    print(i)
+a = input("oquvhi nomi kriting: ")
 
-print("vazifa")
+uqouvchilar = {
+    "sadasd": [1, 12, 21, 12],
+    "jumavoy": [131, 432, 24213],
+}
 
-l1 = "salom"
-for i in l:
-    print(i)
+if a in uqouvchilar:
+    bahao = input("baxni krting: ")
+    try:
+        bahao = int(bahao)
+        if 0 <= bahao <= 5:
+            uqouvchilar[a].append(bahao)
+            print(f"{bahao} bax qoshlidi.")
+        else:
+            print("0...5 orasid bah kriting")
+    except:
+        print("raqam yozsan kn")
 
-print("vazifa")
-
-meva = ["olma","banan", "uzum"]
-print(meva)
-
-print("vazifa")
-
-l2 = [1,2,3,4,5,6,7,8,9,10]
-r = [1,3,5,7,9]
-for i in l2:
-    if i in r:
-        print(f"{i} raqami tog")
-
-print("vazifa")
-
-n = int(input("son kriniting:"))
-d = n ** 2
-print(f"natija{d}")
-
-print("vazifa")
-
-n2 = int(input("son kriniting:"))
-n1 = int(input("son kriniting:"))
-d = n1 + n2
-print(f"natija{d}")
-
-print("vazifa")
-
-r1 = ["ali", "vali","hasan"]
-print(r1.index("hasan"))
+elif a != uqouvchilar:
+    ok = input("bu oquvhchi yoq, qoshsinmi? ha/yoq: ")
+    if ok == "ha":
+        q = input("oquvchini ismini bering: ")
+        bk = input("baxni yoz: ")
+        try:
+            bk = int(bk)
+            if 0 <= bk <= 5:
+                uqouvchilar[q] = [bk]
+                print(uqouvchilar[q])
+            else:
+                print("bah 0 bilan 5 orasda bolsinda")
+        except:
+            print("raqam yoz, boshqa narsa emas")
+    else:
+        print("dastr tuktatild")
--- parol.py
+++ parol.py
@@ -1,9 +1,27 @@
-while True:
-      a = input("soz kiriting: ")
-      soz  = []
+a = input("O‘quvchi nomini kiriting: ")
 
-      if a == a[::-1]:
-          soz.append(a)
-          print(soz)
-      else:
-          print(f"{a} bu soz tneg emas")
+uqouvchilar = {
+    "sadasd": [1, 12, 21, 12],
+    "jumavoy": [131, 432, 24213],
+}
+
+
+if a in uqouvchilar:
+    bahao = input("Bahoni kiriting: ")
+    print(f"{bahao} baho qo‘shildi.")
+    uqouvchilar[a] = [bahao]
+
+elif a != uqouvchilar:
+    ok = input("bunday oquvchi topilmadi oquvchi simni kirishni isasizmi: ")
+    if ok == "ha":
+          q = input("oquvchi ismni kiring kiritng: ")
+          bk = input("bahoni kiring kiritng: ")
+          print(f"{bk} baho kkiritildi")
+          if  type(bk) == int:
+              uqouvchilar[q] = [bk]
+              print(uqouvchilar[q])
+          elif type(bk) == str:
+              print("iltimos faqat raqam kiritng : ")
+    else:
+        print("dastur tohtatiladi")
+print(uqouvchilar)
