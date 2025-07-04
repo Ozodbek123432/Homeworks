b052eb8c4db78628452c56e9f2e78cc04a67633d
ozodbek <sabdumajid91@gmail.com>
2025-07-04 16:47:46 +0500

    bu kod

--- amali vazifa.py
+++ amali vazifa.py
@@ -1,45 +1,23 @@
-def text_to_binary(text):
-    binary = ""
-    for char in text:
-        ascii_value = ord(char)  # Harfni raqamga
-        binary_char = bin(ascii_value)[2:]  # Raqamni 2-likka
-        binary_char = binary_char.zfill(8)  # 8 bit qilib to‘ldirish
-        binary += binary_char + " "
-    return binary.strip()
+from telebot import TeleBot, types
+import random
+bot = TeleBot(token="8027965474:AAGab8Wq-nNjdwnfbUWUajGlV2s57pwFzD0")
 
+@bot.message_handler(commands=['start'])
+def start(xabar: types.Message):
+    bot.send_message(xabar.from_user.id, "salom bu bot tayr")
+    m = bot.send_message(xabar.from_user.id, "Raqam kiriting")
+    bot.register_next_step_handler(xabar, t)
 
-def binary_to_text(binary_input):
-    letters = binary_input.split()
-    text = ""
-    for binary_char in letters:
-        ascii_value = int(binary_char, 2)  # 2-likdan 10-likka
-        harf = chr(ascii_value)  # Raqamdan harfga
-        text += harf
-    return text
+def t(xabar: types.Message):
+    raqam = random.randint(1, 100)
+    print(raqam)
+    if xabar.from_user.id == raqam:
+        bot.send_message(xabar.from_user.id, "To'g'ri topdingiz")
+    elif xabar.from_user.id < raqam:
+        bot.send_message(xabar.from_user.id, "son Kichik")
+    elif xabar.from_user.id > raqam:
+        bot.send_message(xabar.from_user.id, "son Katta")
 
+    bot.register_next_step_handler(xabar, t)
 
-# === Cheksiz menyu ===
-while True:
-    print("\n🧠 2-lik Konvertor")
-    print("1. Matnni 2-likga o‘tkazish")
-    print("2. 2-likni matnga o‘tkazish")
-    print("3. Chiqish")
-
-    tanlov = input("Tanlang (1, 2 yoki 3): ")
-
-    if tanlov == "1":
-        soz = input("So‘z kiriting: ")
-        natija = text_to_binary(soz)
-        print("Ikkilik (2-lik):", natija)
-
-    elif tanlov == "2":
-        binary = input("0 va 1’lardan iborat kod kiriting (har harf orasida bo‘sh joy):\n")
-        natija = binary_to_text(binary)
-        print("Matn:", natija)
-
-    elif tanlov == "3":
-        print("Dastur yakunlandi. 👋")
-        break
-
-    else:
-        print("❌ Noto‘g‘ri tanlov! Faqat 1, 2 yoki 3 ni kiriting.")
+bot.polling()
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-a = input("oquvhi nomi kriting: ")
-
-uqouvchilar = {
-    "sadasd": [1, 12, 21, 12],
-    "jumavoy": [131, 432, 24213],
-}
-
-if a in uqouvchilar:
-    bahao = input("baxni krting: ")
-    try:
-        bahao = int(bahao)
-        if 0 <= bahao <= 5:
-            uqouvchilar[a].append(bahao)
-            print(f"{bahao} bax qoshlidi.")
-        else:
-            print("0...5 orasid bah kriting")
-    except:
-        print("raqam yozsan kn")
-
-elif a != uqouvchilar:
-    ok = input("bu oquvhchi yoq, qoshsinmi? ha/yoq: ")
-    if ok == "ha":
-        q = input("oquvchini ismini bering: ")
-        bk = input("baxni yoz: ")
-        try:
-            bk = int(bk)
-            if 0 <= bk <= 5:
-                uqouvchilar[q] = [bk]
-                print(uqouvchilar[q])
-            else:
-                print("bah 0 bilan 5 orasda bolsinda")
-        except:
-            print("raqam yoz, boshqa narsa emas")
-    else:
-        print("dastr tuktatild")
+def t(son1, son2):
+    print( son1+son2)
+    print( son1-son2)
+    print( son1//son2)
+    print( son1*son2)
+print(t(12,24))
+print(t(23,21))
+print(t(12,12))
+print(t(12,12))
