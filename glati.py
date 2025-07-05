bdaa84976ce7ef935462822aa25a64cdb3863d2d
ozodbek <sabdumajid91@gmail.com>
2025-07-05 15:58:42 +0500

    first commit

--- glati.py
+++ glati.py
@@ -1,8 +1,8 @@
-def t(soz):
-   if soz <= 1:
-       return True
-   elif soz > 1:
-       return False
-print(t(5))
-print(t(0))
-print(t(-2))
+def a(s, q):
+     print(s-q)
+     print(s+q)
+     print(s*q)
+     print(s/q)
+     print(s//q)
+     print(s%q)
+print(a(12,21))
--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-def t(son1, son2):
-    print( son1+son2)
-    print( son1-son2)
-    print( son1//son2)
-    print( son1*son2)
-print(t(12,24))
-print(t(23,21))
-print(t(12,12))
-print(t(12,12))
+from glati import *
+print(a(12,21))
