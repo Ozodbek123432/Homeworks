02e1b35700bbbadb9f6ded7e4f7e4002ed2755c5
ozodbek <sabdumajid91@gmail.com>
2025-07-08 14:33:47 +0500

    add examples

--- amali vazifa.py
+++ amali vazifa.py
@@ -1,23 +1,33 @@
-from telebot import TeleBot, types
-import random
-bot = TeleBot(token="8027965474:AAGab8Wq-nNjdwnfbUWUajGlV2s57pwFzD0")
-
-@bot.message_handler(commands=['start'])
-def start(xabar: types.Message):
-    bot.send_message(xabar.from_user.id, "salom bu bot tayr")
-    m = bot.send_message(xabar.from_user.id, "Raqam kiriting")
-    bot.register_next_step_handler(xabar, t)
-
-def t(xabar: types.Message):
-    raqam = random.randint(1, 100)
-    print(raqam)
-    if xabar.from_user.id == raqam:
-        bot.send_message(xabar.from_user.id, "To'g'ri topdingiz")
-    elif xabar.from_user.id < raqam:
-        bot.send_message(xabar.from_user.id, "son Kichik")
-    elif xabar.from_user.id > raqam:
-        bot.send_message(xabar.from_user.id, "son Katta")
-
-    bot.register_next_step_handler(xabar, t)
-
-bot.polling()
+# from telebot import TeleBot, types
+# import random
+# bot = TeleBot(token="8027965474:AAGab8Wq-nNjdwnfbUWUajGlV2s57pwFzD0")
+#
+# @bot.message_handler(commands=["start"])
+# def start(msg: types.Message):
+#     bot.send_message(msg.chat.id, "So‘z kiriting:")
+#     bot.register_next_step_handler(msg, oquchi)
+#
+# def oquchi(message):
+#     fd = message.text
+#     bot.send_message(message.chat.id, "oyin oynashni holaysizmi ha/yoq")
+#     if message.text == "ha":
+#         bot.send_message(message.chat.id, "holganson tanlang agar oyangan sozingiz meni sonimbilan 1 hil bolsa siz yutasiz")
+#         raqam = message.
+#         print(raqam)
+#         rando = random.randint(1,10)
+#         bot.register_next_step_handler(message, find_the_number, raqam, rando)
+#         print(rando)
+#
+#     if message.text == "yuq":
+#         bot.register_next_step_handler(qk, start)
+#
+# def find_the_number(qk: types.Message, raqam, rando):
+#     if rando == raqam:
+#         bot.send_message(qk.chat.id, "Siz topdingiz.")
+#         bot.register_next_step_handler(qk, start)
+#
+#     else:
+#         bot.send_message(qk.chat.id, "noto‘g‘ri")
+#         oquchi(qk)
+#
+# bot.polling()
--- glati.py
+++ glati.py
@@ -1,8 +1,53 @@
-def a(s, q):
-     print(s-q)
-     print(s+q)
-     print(s*q)
-     print(s/q)
-     print(s//q)
-     print(s%q)
-print(a(12,21))
+import pygame
+import sys
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bosganda Ishlaydigan O‘yin")
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+ball_pos = [400, 300]
+ball_radius = 30
+ball_speed = [4, 4]
+moving = False  # To‘p harakatlanyaptimi yoki yo‘q
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(100)
+    window.fill(WHITE)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = True
+
+        # Foydalanuvchi sichqoncha bosganini tekshirish
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            # To‘pga tegildimi (masofa bo‘yicha)
+            distance = math.sqrt((mouse_x - ball_pos[0])**2 + (mouse_y - ball_pos[1])**2)
+            if distance <= ball_radius:
+                moving = True
+
+    # Agar bosilgan bo‘lsa to‘p harakatlanadi
+    if moving:
+        ball_pos[0] += ball_speed[0]
+        ball_pos[1] += ball_speed[1]
+
+        # Chegaralarga tegsa orqaga qaytarish
+        if ball_pos[0] - ball_radius <= 0 or ball_pos[0] + ball_radius >= WIDTH:
+            ball_speed[0] *= -1
+        if ball_pos[1] - ball_radius <= 0 or ball_pos[1] + ball_radius >= HEIGHT:
+            ball_speed[1] *= -1
+
+    pygame.draw.circle(window, RED, ball_pos, ball_radius)
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
